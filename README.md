ae025722beac6b625e3a761b63beaaafb9d98e98
suvankars <suvankar.17@gmail.com>
2021-07-18 02:50:21 +0530

    adding gatsby theme portfolio

---
+++ README.md
@@ -0,0 +1,32 @@
+# Portfolio
+
+### Locally
+
+1. Create an account at [Formium](https://formium.io)
+2. Grab a Google recaptcha key from [Google Recaptcha](https://www.google.com/recaptcha/admin)
+3. Grab your Github token from GitHub
+4. Run `cp .env.development.template .env.development`
+5. Run `npm i && npm start`
+
+
+### Clean the cache
+
+This removes the `.cache/` & `public/` folders
+
+```bash
+yarn reset
+```
+
+## Built with
+
+- Adobe XD
+- Gatsby
+- React & GraphQL
+- Formium
+- Google recaptcha
+- VSCode
+- And these useful of JavaScript libraries & Gatsby plugins [package.json](package.json)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for more details
---
+++ src/data/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  defaultTitle: 'Suvankar Satpati',
+  logo: '',
+  author: 'Suvankar Satpati',
+  url: 'https://suvankar.vercel.app/',
+  legalName: 'John Doe',
+  defaultDescription: 'I’m Suvankar Satpati and I’m a Backend & Frontend engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/suvankar.17',
+    github: 'https://github.com/suvankars',
+    linkedin: 'https://www.linkedin.com/in/suvankarsatpati/',
+  },
+  googleAnalyticsID: 'G-78D6RF7MQD',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@suvankar17',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2018',
+};
